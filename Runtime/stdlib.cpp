018960a6ba1234be2a4e0af34bff717f274269fb
Sven Bieg <svenbieg@web.de>
2024-08-05 18:59:47 +0200

    Not implemented

--- Runtime/stdlib.cpp
+++ Runtime/stdlib.cpp
@@ -22,35 +22,27 @@ _reent* _impure_ptr=nullptr;
 
 extern "C" INT fputc(INT c, FILE* file)
 {
-return fwrite(&c, 1, 1, file);
+throw NotImplementedException();
 }
 
 extern "C" INT fputs(LPCSTR str, FILE* file)
 {
-INT len=0;
-while(str[len])
-	{
-	size_t written=fwrite(&str[len], 1, 1, file);
-	if(!written)
-		break;
-	len++;
-	}
-return len;
+throw NotImplementedException();
 }
 
 extern "C" size_t fwrite(VOID const* buf, size_t size, size_t count, FILE* file)
 {
-return 0;
+throw NotImplementedException();
 }
 
 extern "C" int sprintf(LPSTR str, LPCSTR format, ...)
 {
-return 0;
+throw NotImplementedException();
 }
 
 extern "C" size_t write(FILE* file, void const* buf, size_t size)
 {
-return fwrite(buf, 1, size, file);
+throw NotImplementedException();
 }
 
 
@@ -118,74 +110,27 @@ return dst;
 
 extern "C" LPCSTR strchr(LPCSTR str, INT c)
 {
-if(!str)
-	return nullptr;
-while(str[0]!=c)
-	str++;
-return str;
+throw NotImplementedException();
 }
 
 extern "C" INT strcmp(LPCSTR str1, LPCSTR str2)
 {
-if(!str1)
-	{
-	if(!str2)
-		return 0;
-	return -1;
-	}
-if(!str2)
-	return 1;
-for(UINT pos=0; pos>=0; pos++)
-	{
-	if(str1[pos]>str2[pos])
-		return 1;
-	if(str1[pos]<str2[pos])
-		return -1;
-	if(!str1[pos])
-		break;
-	}
-return 0;
+throw NotImplementedException();
 }
 
 extern "C" INT strlen(LPCSTR str)
 {
-if(!str)
-	return 0;
-UINT len=0;
-for(; str[len]; len++);
-return len;
+throw NotImplementedException();
 }
 
 extern "C" INT strncmp(LPCSTR str1, LPCSTR str2, UINT len)
 {
-if(!str1)
-	{
-	if(!str2)
-		return 0;
-	return -1;
-	}
-if(!str2)
-	return 1;
-for(UINT pos=0; pos<len; pos++)
-	{
-	if(str1[pos]>str2[pos])
-		return 1;
-	if(str1[pos]<str2[pos])
-		return -1;
-	if(!str1[pos])
-		break;
-	}
-return 0;
+	throw NotImplementedException();
 }
 
 extern "C" UINT strtoul(LPCSTR str, LPCSTR end, INT base)
 {
-UINT len=0;
-if(end>str)
-	len=(UINT)(end-str);
-UINT value=0;
-StringScanUInt(str, &value, base, len);
-return value;
+throw NotImplementedException();
 }
 
 
