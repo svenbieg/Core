1b4c5e5fc5301b1410ae57d0a3cffc6a6ff5fa84
Sven Bieg <svenbieg@web.de>
2024-10-04 18:21:40 +0200

    Fixed TaskLock

--- Concurrency/TaskLock.h
+++ Concurrency/TaskLock.h
@@ -27,18 +27,15 @@ class TaskLock: public ScopedLock
 {
 public:
 	// Con-/Destructors
-	inline TaskLock(Mutex& Mutex): m_Mutex(&Mutex)
+	inline TaskLock(Mutex& Mutex)
 		{
+		m_Mutex=&Mutex;
 		m_Mutex->LockBlocking();
 		}
 
 	// Common
 	inline VOID Lock()override { m_Mutex->LockBlocking(); }
 	inline BOOL TryLock()override { return m_Mutex->TryLockBlocking(); }
-
-private:
-	// Common
-	Mutex* m_Mutex;
 };
 
 }
