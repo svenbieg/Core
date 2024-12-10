017ba09f4c8b652111c90a75937074947b38baf3
Sven Bieg <svenbieg@web.de>
2024-12-10 18:22:34 +0100

    Updated Scheduler
    Sleeping is not allowed in the main-task

--- TimeHelper.h
+++ TimeHelper.h
@@ -30,11 +30,14 @@ return Devices::Timers::SystemTimer::Milliseconds64();
 inline VOID Sleep(UINT Milliseconds)
 {
 using Scheduler=Concurrency::Scheduler;
+assert(!Scheduler::IsMainTask()); // Sleeping is not allowed in the main-task
 Scheduler::SuspendCurrentTask(Milliseconds);
 }
 
 inline VOID SleepMicroseconds(UINT Microseconds)
 {
+using Scheduler=Concurrency::Scheduler;
+assert(!Scheduler::IsMainTask()); // Sleeping is not allowed in the main-task
 using SystemTimer=Devices::Timers::SystemTimer;
 UINT64 end=SystemTimer::Microseconds64()+Microseconds;
 while(SystemTimer::Microseconds64()<=end);
