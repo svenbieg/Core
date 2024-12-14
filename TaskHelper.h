5d02794d1dbbc60e3cf2f23387790517b4bfddf8
Sven Bieg <svenbieg@web.de>
2024-12-14 20:07:56 +0100

    Added MainTask

--- TaskHelper.h
+++ TaskHelper.h
@@ -9,7 +9,6 @@
 // Using
 //=======
 
-#include "Concurrency/DispatchedQueue.h"
 #include "Concurrency/Scheduler.h"
 #include "Concurrency/ScopedLock.h"
 #include "Concurrency/SharedLock.h"
@@ -55,27 +54,4 @@ return task;
 inline Handle<Concurrency::Task> GetCurrentTask()
 {
 return Concurrency::Scheduler::GetCurrentTask();
-}
-
-
-//==========
-// Dispatch
-//==========
-
-inline VOID Dispatch(VOID (*Procedure)())
-{
-auto handler=new Concurrency::DispatchedProcedure(Procedure);
-Concurrency::DispatchedQueue::Append(handler);
-}
-
-template <class _owner_t> inline VOID Dispatch(_owner_t* Owner, VOID (_owner_t::*Procedure)())
-{
-auto handler=new Concurrency::DispatchedMemberProcedure<_owner_t>(Owner, Procedure);
-Concurrency::DispatchedQueue::Append(handler);
-}
-
-template <class _owner_t, class _lambda_t> inline VOID Dispatch(_owner_t* Owner, _lambda_t&& Lambda)
-{
-auto handler=new Concurrency::DispatchedLambda<_owner_t, _lambda_t>(Owner, std::forward<_lambda_t>(Lambda));
-Concurrency::DispatchedQueue::Append(handler);
 }
